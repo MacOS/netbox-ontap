f816ba27860ec9885c9f49897d400daa7c614cd2
Leonhard Kreißig <leonhard.kreissig@telekom.de>
2026-02-06 17:14:16 +0100

    (Fix) don't require Tenant and StoragePool on LUN via API

--- netbox_storage/api/serializers.py
+++ netbox_storage/api/serializers.py
@@ -29,8 +29,8 @@ class LUNSerializer(NetBoxModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='plugins-api:netbox_storage-api:lun-detail'
     )
-    storage_pool = StoragePoolSerializer(nested=True)
-    tenant = TenantSerializer(nested=True)
+    storage_pool = StoragePoolSerializer(nested=True, allow_null=True)
+    tenant = TenantSerializer(nested=True, allow_null=True)
 
     class Meta:
         model = LUN
@@ -48,7 +48,7 @@ class QuotaSerializer(NetBoxModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='plugins-api:netbox_storage-api:quota-detail'
     )
-    tenant = TenantSerializer(nested=True)
+    tenant = TenantSerializer(nested=True, allow_null=True)
 
     class Meta:
         model = Quota
