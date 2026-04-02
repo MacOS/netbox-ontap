41100304aa7d78e353bb1346396b69c15fbf3043
Leonhard Kreißig <leonhard.kreissig@telekom.de>
2026-04-02 11:16:48 +0000

    Release Version 1.1.0

--- netbox_ontap/__init__.py
+++ netbox_ontap/__init__.py
@@ -4,10 +4,10 @@ class NetBoxOntapConfig(PluginConfig):
     name = 'netbox_ontap'
     verbose_name = ' NetBox ONTAP'
     description = 'Netbox ONTAP Storage Administration Plugin'
-    version = '1.0.0'
+    version = '1.1.0'
     base_url = 'ontap'
     min_version = "4.3.0"
-    author = 'Gabor Somogyvari'
+    author = 'Gabor Somogyvari, Leonhard Kreißig'
 
 
 config = NetBoxOntapConfig
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='netbox-ontap-plugin',
-    version='1.0.0',
+    version='1.1.0',
     description='NetBox ONTAP plugin',
     long_description=long_description,
     long_description_content_type='text/markdown',
